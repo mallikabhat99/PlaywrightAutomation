9c4c3ef6efc67fd0e41620ff654a885df5461ac8
mallikabhat99 <mallikabhat99@gmail.com>
2025-10-27 14:27:53 +0530

    special locators

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,6 +1,6 @@
 const {test,expect} = require('@playwright/test');
 
-test.only('Page fixture playwright test', async({page})=>{
+test('Page fixture playwright test', async({page})=>{
     const userName = page.locator('#userEmail');
     const signInButton = page.locator('#login');
     const titles = page.locator(".card-body b");
@@ -52,7 +52,18 @@ test.only('Page fixture playwright test', async({page})=>{
     console.log(orderId);
 
 
+    await page.locator('button[routerlink*="myorders"]').click();
+    await page.locator('tbody').waitFor();
 
-
+    const rows = await page.locator('tbody tr');
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
 });
---
+++ tests/SpecialLocators.spec.js
@@ -0,0 +1,18 @@
+const {test,expect} = require("@playwright/test");
+
+test('Playwright special locators', async({page})=>{
+
+    await page.goto('https://rahulshettyacademy.com/angularpractice/');
+    await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByLabel('Employed').check();
+    await page.getByLabel('Gender').selectOption("Female");
+
+    await page.getByPlaceholder('Password').fill("Some password");
+    await page.getByRole("button",{name:"Submit"}).click();
+  const bool =  await page.getByText('Success! The Form has been submitted successfully!.').isVisible();
+  expect(bool).toBeTruthy();
+
+  await page.getByRole("link" ,{name : "Shop"}).click();
+  await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click();
+
+})
